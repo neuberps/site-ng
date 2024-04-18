6360588a29507738db82250430dcd253dc459b11
Fanaina Alves <fanaiina.allves@gmail.com>
2024-04-18 11:34:03 -0300

    criando o primeiro card para o serviço

--- src/app/service/service-routing.module.ts
+++ src/app/service/service-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServiceComponent } from './service/service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'service', component: ServiceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/service/service.module.ts
+++ src/app/service/service.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ServiceRoutingModule } from './service-routing.module';
+import { ServiceComponent } from './service/service.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ServiceRoutingModule
+    ServiceRoutingModule,
+    ServiceComponent
+  ],
+  exports:[
+    ServiceComponent
   ]
 })
 export class ServiceModule { }
