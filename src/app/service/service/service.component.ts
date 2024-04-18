6be92586a5ff288ae9399769a6ca49b2ba28feb9
Fanaina Alves <fanaiina.allves@gmail.com>
2024-04-18 17:11:37 -0300

    código para gerar o card a medida que vai clicando no botão de adicionar serviço.

--- src/app/service/service/service.component.ts
+++ src/app/service/service/service.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-service',
@@ -8,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrl: './service.component.css'
 })
 export class ServiceComponent {
+  criarServico(): void {
+    // Crie um novo elemento div para o card
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+    cardDiv.style.width = '20rem';
 
+    // Crie o conteúdo do card
+    cardDiv.innerHTML = `
+
+        <div class="card-body" style="width: 20rem; height: 15rem; text-align: center; ">
+          <h5 class="card-title">Título do Serviço</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Categoria do Serviço</h6>
+          <p class="card-text">Um exemplo de texto rápido para preencher o conteúdo do card.Um exemplo de texto rápido para preencher o conteúdo do card.</p>
+          <button class="card-link btn btn-outline-primary">Adicionar Serviço</button>
+        </div>
+
+    `;
+
+    // Adicione o card à página (você pode anexá-lo a um contêiner específico)
+    //document.body.appendChild(cardDiv);
+
+    // Encontre o contêiner específico (por ID) e anexe o card a ele
+    const meuContainer = document.getElementById('meuContainer');
+    if (meuContainer) {
+      meuContainer.appendChild(cardDiv);
+    }
+  }
 }
