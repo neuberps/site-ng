98773d66f3f7138a5c51110fd45fb0e4583c2ce8
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2024-04-22 17:16:54 -0300

    Create login moldel and update name shopping-card

---
+++ src/app/pages/shopping-card/shopping-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-card',
+  templateUrl: './shopping-card.component.html',
+  styleUrl: './shopping-card.component.css'
+})
+export class ShoppingCardComponent {
+
+}
