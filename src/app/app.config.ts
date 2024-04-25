073d1c2c82c577bae88061a8f1f263bbf3a84159
PriscilaCPassos <116843833+PriscilaCPassos@users.noreply.github.com>
2024-04-25 13:42:23 -0300

    Importing components in app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,17 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimations(),
+    provideToastr(),
+    provideHttpClient(withFetch())
+
+  ]
 };
