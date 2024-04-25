ad4f0a5b7c8376cfe15dccb64497fd5b887aecbd
PriscilaCPassos <116843833+PriscilaCPassos@users.noreply.github.com>
2024-04-25 13:56:07 -0300

    Login function implementation

--- src/app/template/navbar/navbar.component.ts
+++ src/app/template/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,5 +10,12 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  constructor(private router: Router) { }
+
+  hideComponent():  boolean {
+    const url = this.router.url;
+    return url === '/login' || url === '/signup';
+  }
 
 }
