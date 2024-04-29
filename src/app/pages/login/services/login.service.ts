7a65e96a2b7b5c813771390bc0888ef9d0fc0a49
PriscilaCPassos <116843833+PriscilaCPassos@users.noreply.github.com>
2024-04-29 17:28:45 -0300

    Updating login.services

--- src/app/pages/login/services/login.service.ts
+++ src/app/pages/login/services/login.service.ts
@@ -1,33 +1,67 @@
+import { User } from './../model/user';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginResponse } from '../types/login';
 import { tap } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  apiUrl: string = "http://localhost:9008/auth"
+  apiUrl: string = "http://localhost:9008/auth";
+  private SESSION_USER_KEY = 'session_user';
+  private SESSION_TOKEN_KEY = 'session_token';
 
   constructor( private httpClient: HttpClient) { }
+
+  // Método para limpar os dados da sessão
+  clean(): void {
+    window.sessionStorage.clear();
+  }
+
+  // Método para limpar os dados do formulário
+  clearFormData(): void {}
+
 
   // Função login:
   login(email: string, password: string){
     return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
       tap((value) => {
-      sessionStorage.setItem("auth-token",value.token)
-      sessionStorage.setItem("username",value.username)
+        this.saveUserDataInSession(value.token, value.user);
     })
     )
   }
 
-  signup(username: string, email: string, password: string){
-    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { username, email, password }).pipe(
+// Função Cadastrar
+  signup(name: string, email: string, username: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, username, password }).pipe(
       tap((value) => {
-      sessionStorage.setItem("auth-token",value.token)
-      sessionStorage.setItem("username",value.username)
+        this.saveUserDataInSession(value.token, value.user);
     })
     )
   }
+
+  // Método privado (Armazena os dados relevantes na sessão do usuário)
+  private saveUserDataInSession(token: string, user: User) {
+    sessionStorage.setItem(this.SESSION_TOKEN_KEY, token);
+    sessionStorage.setItem(this.SESSION_USER_KEY, JSON.stringify(user));
+
+  }
+
+  // Método que verifica se o usuário está autenticado na apliação.
+  public isLoggedIn(): boolean {
+    const user = window.sessionStorage.getItem(this.SESSION_USER_KEY);
+    if (user) {
+      return true;
+    }
+    return false;
+  }
+
+  // Método para atualizar a página
+  reloadPage(): void {
+    window.location.reload();
+  }
+
 }
