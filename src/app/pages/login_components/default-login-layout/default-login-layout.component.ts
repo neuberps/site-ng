5240f0ddbf21a1a6cd3833ad8829b4ce05284733
PriscilaCPassos <116843833+PriscilaCPassos@users.noreply.github.com>
2024-05-02 13:57:45 -0300

    Updating the logic on component

--- src/app/pages/login_components/default-login-layout/default-login-layout.component.ts
+++ src/app/pages/login_components/default-login-layout/default-login-layout.component.ts
@@ -1,14 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-default-login-layout',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './default-login-layout.component.html',
   styleUrl: './default-login-layout.component.css'
 })
-export class DefaultLoginLayoutComponent {
+export class DefaultLoginLayoutComponent implements OnInit{
+
+  constructor(private router: Router) {}
+
+  currentScreen: string = 'login'; 
 
   @Input() title: string = "";
   @Input() primaryBtnText: string = "";
@@ -16,6 +22,16 @@ export class DefaultLoginLayoutComponent {
   @Input() disablePrimaryBtn: boolean = true;
   @Output("submit") onSubmit = new EventEmitter();
   @Output("navigate") onNavigate = new EventEmitter();
+
+
+  ngOnInit(): void {
+    this.setCurrentScreen();
+  }
+
+  setCurrentScreen(): void {
+    const currentUrl = this.router.url;
+    this.currentScreen = currentUrl.includes('signup') ? 'signup' : 'login';
+  }
 
 
   submit() {
