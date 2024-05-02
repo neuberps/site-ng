39b393aed7f2650091e16dcb02c8a98d1a97a4c7
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2024-05-02 17:28:22 -0300

    Update order and navbar

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -1,29 +1,13 @@
-<<<<<<< Updated upstream
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-=======
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Order } from './model/order';
-import { OrderService } from './service/order.service';
-import { Router, RouterModule } from '@angular/router';
+import {  RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { routes } from '../../app.routes';
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-order',
   standalone: true,
-<<<<<<< Updated upstream
-  imports: [ RouterLink ],
-  templateUrl: './order.component.html',
-  styleUrl: './order.component.css'
-})
-export class OrderComponent {
-
-}
-=======
   imports: [ CommonModule, FormsModule,
     HttpClientModule, RouterModule],
   templateUrl: './order.component.html',
@@ -47,4 +31,3 @@ export class OrderComponent implements OnInit {
   }
 }
 
->>>>>>> Stashed changes
--- src/app/template/navbar/navbar.component.ts
+++ src/app/template/navbar/navbar.component.ts
@@ -1,34 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { CartService } from '../../pages/shopping-card/services/cart.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   contador: number = 0;
 
+  constructor(private router: Router, private cartService: CartService) {}
 
-  constructor(private router: Router) { }
-
-  hideComponent():  boolean {
+  hideComponent(): boolean {
     const url = this.router.url;
-    return url === '/login' || url === '/signup' || url === '/order' || url === '/order-detail';
+    return (
+      url === '/login' ||
+      url === '/signup' ||
+      url === '/order' ||
+      url === '/order-detail'
+    );
   }
 
-  constructor(private cartService: CartService) { }
-
   ngOnInit() {
-      this.cartService.currentCount.subscribe(value => {
+    this.cartService.currentCount.subscribe((value) => {
       this.contador = value;
 
-      console.log(this.contador)
+      console.log(this.contador);
     });
-}
-
+  }
 }
